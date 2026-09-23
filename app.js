d5e333da55c6137228d4c78bab71638c30adb54e
kunal-vats <kvats8701@gmail.com>
2026-09-23 18:43:35 +0530

    Add campgrounds index route and view

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(() => console.log('Database connected'))
@@ -15,6 +16,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
 });
 
 app.listen(3000, () => {
